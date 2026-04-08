84d77762a9aa9f910bf4dc494c0209bd4cb940ed
cosmicdanish1 <cosmicdanish@outlook.com>
2026-04-08 13:03:26 +0530

    fix: reports.controller.ts corrupted import keyword

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-rmimport { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ReportsService } from './reports.service';
 
