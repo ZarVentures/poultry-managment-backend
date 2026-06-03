a2473311af6872f29878ed041f7e031bcad2ee62
Gautamchaudhari345 <gautamchaudahri345@gmail.com>
2026-06-03 16:00:40 +0530

    fix

--- src/payment-vouchers/dto/update-payment-voucher.dto.ts
+++ src/payment-vouchers/dto/update-payment-voucher.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePaymentVoucherDto } from './create-payment-voucher.dto';
+import { IsOptional, IsEnum } from 'class-validator';
 
-export class UpdatePaymentVoucherDto extends PartialType(CreatePaymentVoucherDto) {}
+export class UpdatePaymentVoucherDto extends PartialType(CreatePaymentVoucherDto) {
+  // Make voucherType optional for updates
+  @IsOptional()
+  @IsEnum(['in', 'out'])
+  voucherType?: 'in' | 'out';
+}
--- src/payment-vouchers/payment-vouchers.module.ts
+++ src/payment-vouchers/payment-vouchers.module.ts
@@ -4,14 +4,19 @@ import { PaymentVouchersService } from './payment-vouchers.service';
 import { PaymentVouchersController } from './payment-vouchers.controller';
 import { BillingModule } from '../billing/billing.module';
 import { PaymentVoucher } from './payment-voucher.entity';
+import { RetailersModule } from '../retailers/retailers.module';
+import { FarmersModule } from '../farmers/farmers.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([PaymentVoucher]),
     BillingModule,
+    RetailersModule,
+    FarmersModule,
   ],
   controllers: [PaymentVouchersController],
   providers: [PaymentVouchersService],
   exports: [PaymentVouchersService],
 })
 export class PaymentVouchersModule { }
+
